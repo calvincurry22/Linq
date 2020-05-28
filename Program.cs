5b4d933b63fd89a30fdee1e4f2b20e0fdcf8ca09
calvincurry22 <calvincurry22@gmail.com>
2020-05-28 16:13:19 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<string> fruits = new List<string>
+            {
+                "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"
+            };
+            IEnumerable<string> LFruits = fruits.Where(fruit => fruit.StartsWith("L"));
+            LFruits.ToList().ForEach(fruit => Console.WriteLine(fruit));
+        }
+    }
+}
